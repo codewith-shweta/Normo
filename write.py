71e8ef0ee50af1ffb7176297dd272bd20ea8ca32
shwet09 <shwetkushwaha009@gmail.com>
2025-06-15 04:09:17 +0530

    add files

---
+++ write.py
@@ -0,0 +1,8 @@
+import csv
+
+with open('data.csv', mode='w') as csvfile:
+    fieldnames = ['name', 'email', 'contact', 'profession', 'country', 'keycode']
+    writes =csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writes.writeheader()
+    writes.writerow({"name": "john", "email": "john123@gmail.com", "contact": 3434343, "profession": "Tech", "country": "India", "keycode": "08"})
+    writes.writerow({"name": "ron", "email": "ron123@gmail.com", "contact": 478348378, "profession": "Tech", "country": "Us", "keycode": "03"})
