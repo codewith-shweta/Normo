c55e498557b51be8c9d9a339fa9639e92af12ab7
shwet09 <shwetkushwaha009@gmail.com>
2025-06-18 06:07:13 +0530

    intial commit

--- write.py
+++ write.py
@@ -6,3 +6,4 @@ with open('data.csv', mode='w') as csvfile:
     writes.writeheader()
     writes.writerow({"name": "john", "email": "john123@gmail.com", "contact": 3434343, "profession": "Tech", "country": "India", "keycode": "08"})
     writes.writerow({"name": "ron", "email": "ron123@gmail.com", "contact": 478348378, "profession": "Tech", "country": "Us", "keycode": "03"})
+    writes.writerow({"name": "nehonz", "email": "neo123@gmail.com", "contact": 2903030, "profession": "Software Developer", "country": "Canada", "keycode": "05"})
